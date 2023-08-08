c0b257713cc984143934c40a858b93c9cab97d31
Abhay Vachhani <abhayvachhani28@gmail.com>
2023-08-08 13:32:13 +0530

    Created readme generator and github workflow

---
+++ generate_readme.py
@@ -0,0 +1,13 @@
+import os
+from urllib import parse
+
+ignoreList = ".git .gitignore generate_readme.py readme.md".split()
+
+text = "# Leetcode Solutions\n\n"
+
+for fileName in sorted(os.listdir()):
+    if fileName.lower() not in ignoreList:
+        text += f"[{fileName}]({parse.quote(fileName)})\n\n"
+
+with open("Readme.md", "w") as fp:
+    fp.write(text)
