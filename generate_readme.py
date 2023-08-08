888cc1869a5aa383db5d2aa2cf6b9d2d8a85cee7
Abhay Vachhani <abhayvachhani28@gmail.com>
2023-08-09 05:07:19 +0530

    Added leetcode profile to readme

--- generate_readme.py
+++ generate_readme.py
@@ -4,6 +4,8 @@ from urllib import parse
 ignoreList = ".git .github .gitignore generate_readme.py readme.md".split()
 
 text = "# Leetcode Solutions\n\n"
+text += "Leetcode Profile: https://leetcode.com/abhayvachhani28/\n\n---\n\n"
+# Rank on 2023-08-08: 2,700,474
 
 for fileName in sorted(os.listdir()):
     if fileName.lower() not in ignoreList:
